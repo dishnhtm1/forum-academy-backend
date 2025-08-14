3d9abe4062dcf8d14640454e28526102ab73cc36
Diushan <forum_st03@forum.ac.jp>
2025-08-14 10:10:02 +0900

    ss1

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(cors({
         process.env.CLIENT_URL
     ].filter(Boolean),
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
